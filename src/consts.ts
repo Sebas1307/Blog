5dd80f6ec99b49a2ae12d7e1d96855d5eaab7846
Sebastian Patiño Caro <106630623+Sebas1307@users.noreply.github.com>
2025-05-22 09:25:17 -0500

    Se implementa diseño responsive para mobile

--- src/consts.ts
+++ src/consts.ts
@@ -7,25 +7,24 @@ export const SOCIAL_LINKS = [
    {
       id: 'tiktok',
       href: 'https://www.tiktok.com/@sebastianpatio540?_t=ZS-8wWMAqjj3N2&_r=1',
-      icon: '📱',
-      text: 'TikTok'
+      text: 'TikTok',
+      svg: '/tiktok.svg'
    },
    {
       id: 'instagram',
       href: 'https://www.instagram.com/sebass_caro?igsh=MTNpMHc3bTU4bGptbw%3D%3D&utm_source=qr',
-      icon: '📸',
-      text: 'Instagram'
+      text: 'Instagram',
+      svg: '/instagram.svg'
    },
    // {
    //    id: 'youtube',
    //    href: 'https://x.com/sebastianpa138',
-   //    icon: '🎥',
    //    text: 'YouTube'
    // },
    {
       id: 'twitter',
       href: 'https://x.com/sebastianpa138',
-      icon: '🐦',
-      text: 'X'
+      text: 'X',
+      svg: '/twitter.svg'
    }
 ]
